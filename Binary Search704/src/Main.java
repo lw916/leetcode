e10954cf982073a7a0b05ed3ffa8399edb2fb16b
liwei <lw@blask.cn>
2023-06-12 18:09:03 +0800

    增加一些题目

---
+++ Binary Search704/src/Main.java
@@ -0,0 +1,20 @@
+class Solution {
+    // 二分查找法
+    // 有序升序
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] > target){
+                right = mid -1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
