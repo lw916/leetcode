52bdcb5ecb73a574ec3e52e3055c4b186211087e
lw9916 <1473381720@qq.com>
2023-06-23 15:03:13 +0800

    增加题目

---
+++ Restore IP Addresses93/src/Main.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    static final int SEG_COUNT = 4;
+    List<String> ans = new ArrayList<String>();
+    int[] segments = new int[SEG_COUNT];
+
+    public List<String> restoreIpAddresses(String s) {
+        segments = new int[SEG_COUNT];
+        dfs(s, 0, 0);
+        return ans;
+    }
+
+    public void dfs(String s, int segId, int segStart) {
+        // 如果找到了 4 段 IP 地址并且遍历完了字符串，那么就是一种答案
+        if (segId == SEG_COUNT) {
+            if (segStart == s.length()) {
+                StringBuffer ipAddr = new StringBuffer();
+                for (int i = 0; i < SEG_COUNT; ++i) {
+                    ipAddr.append(segments[i]);
+                    if (i != SEG_COUNT - 1) {
+                        ipAddr.append('.');
+                    }
+                }
+                ans.add(ipAddr.toString());
+            }
+            return;
+        }
+
+        // 如果还没有找到 4 段 IP 地址就已经遍历完了字符串，那么提前回溯
+        if (segStart == s.length()) {
+            return;
+        }
+
+        // 由于不能有前导零，如果当前数字为 0，那么这一段 IP 地址只能为 0
+        if (s.charAt(segStart) == '0') {
+            segments[segId] = 0;
+            dfs(s, segId + 1, segStart + 1);
+        }
+
+        // 一般情况，枚举每一种可能性并递归
+        int addr = 0;
+        for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            addr = addr * 10 + (s.charAt(segEnd) - '0');
+            if (addr > 0 && addr <= 0xFF) {
+                segments[segId] = addr;
+                dfs(s, segId + 1, segEnd + 1);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+class Solution2{
+
+    List<String> result = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+        if (s.length() == 0){
+            return new ArrayList<>();
+        }
+        backTracking(s,0);
+        return result;
+    }
+    public void backTracking(String s, int startIndex){
+        if (path.size()>4){
+            return;
+        }
+        if (path.size() == 4 && startIndex >= s.length()){
+            String temp = String.join(".",path);
+            result.add(temp);
+            return;
+        }
+
+        for (int i = startIndex;i<s.length();i++){
+            if (isValid(s,startIndex,i)){
+                String str = s.substring(startIndex,i+1);
+                path.add(str);
+            }else {
+                continue;
+            }
+            backTracking(s,i+1);
+            path.remove(path.size()-1);
+
+        }
+    }
+    private Boolean isValid(String s, int start, int end) {
+        if (start > end) {
+            return false;
+        }
+        if (s.charAt(start) == '0' && start != end) { // 0开头的数字不合法
+            return false;
+        }
+        int num = 0;
+        for (int i = start; i <= end; i++) {
+            if (s.charAt(i) > '9' || s.charAt(i) < '0') { // 遇到⾮数字字符不合法
+                return false;
+            }
+            num = num * 10 + (s.charAt(i) - '0');
+            if (num > 255) { // 如果⼤于255了不合法
+                return false;
+            }
+        }
+        return true;
+    }
+}
